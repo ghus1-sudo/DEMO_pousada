1b6c0c08ea61b245689f3d92fb594ed59d69ee54
ghus1-sudo <guilhermehaus31@gmail.com>
2026-09-09 17:52:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const zap = document.getElementById("zap");
+
+zap.addEventListener("click", function(){
+    window.open("https://wa.me/554196523656", "_blank");
+});
+
+
+
+
+const sobreImagem = document.querySelector("#sobreImagem");
+
+const imagens = [
+    "pousada_externa.webp",
+    "pousada_externa2.jpg",
+    "pousada_externa3.jpg",
+    "pousada_externa4.jpg"
+];
+
+let indice = 0;
+
+setInterval(function() {
+
+    sobreImagem.classList.add("fade");
+
+    setTimeout(function() {
+
+        indice++;
+
+        if (indice >= imagens.length) {
+            indice = 0;
+        }
+
+        sobreImagem.src = imagens[indice];
+
+        sobreImagem.classList.remove("fade");
+
+    }, 700);
+
+}, 4000);
+
+
+
+
+
+
+const elementos = document.querySelectorAll(
+    ".sobre-content, .sobre-img, .quarto, .experiencia, .galeria-grid img, .localizacao-content, .mapa, .cta-content"
+);
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+}, {
+    threshold: 0.15
+});
+
+
+elementos.forEach((elemento) => {
+
+    observer.observe(elemento);
+
+});
